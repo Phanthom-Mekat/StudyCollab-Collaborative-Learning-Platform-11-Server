003699199c573921bb8c7cc614818c4a8d781663
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-12-24 00:37:06 +0600

    create search added

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(
 // cookie parser middleware
 app.use(cookieParser());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhb6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zhb6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -41,9 +38,13 @@ async function run() {
     // await client.connect();
     // Send a ping to confirm a successful connection
 
-    const assignmentCollection = client.db("GroupStudy").collection("assignment");
-    const userCollection = client.db("GroupStudy").collection('users')
-    const assignmentSubmissionCollection = client.db("GroupStudy").collection('assignmentSubmission')
+    const assignmentCollection = client
+      .db("GroupStudy")
+      .collection("assignment");
+    const userCollection = client.db("GroupStudy").collection("users");
+    const assignmentSubmissionCollection = client
+      .db("GroupStudy")
+      .collection("assignmentSubmission");
 
     // create a new assignment
     app.post("/create", async (req, res) => {
@@ -51,14 +52,14 @@ async function run() {
       const result = await assignmentCollection.insertOne(assignment);
       res.json(result);
     });
-    
+
     // get all assignments
     app.get("/create", async (req, res) => {
       const cursor = assignmentCollection.find({});
       const result = await cursor.toArray();
       res.json(result);
     });
-    
+
     // get a single assignment
     app.get("/create/:id", async (req, res) => {
       const id = req.params.id;
@@ -66,7 +67,7 @@ async function run() {
       const result = await assignmentCollection.findOne(query);
       res.json(result);
     });
-    
+
     app.put("/create/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -84,80 +85,117 @@ async function run() {
       res.json(result);
     });
 
+    // search by defficulty level and also normal search
+    app.get("/create/search/:search", async (req, res) => {
+      const search = req.params.search;
+      const query = {
+        $or: [
+          { title: { $regex: search, $options: "i" } },
+          { description: { $regex: search, $options: "i" } },
+        ],
+      };
 
-    //delete an assignment who created 
-    app.delete('/create/:id',async(req,res)=>{
+      const result = await assignmentCollection.find(query).toArray();
+      res.json(result);
+    });
+
+    app.get("/create/difficulty/:difficultyLevel", async (req, res) => {
+      const { difficultyLevel } = req.params;
+      const query = {};
+    
+      if (difficultyLevel !== "all") {
+        query.difficultyLevel = difficultyLevel;
+      }
+    
+      const result = await assignmentCollection.find(query).toArray();
+      res.json(result);
+    });
+
+    app.get("/create/search/difficulty/:difficultyLevel/:search", async (req, res) => {
+      const search = req.params.search;
+      const difficultyLevel = req.params.difficultyLevel;
+      const query = {
+        $or: [
+          { title: { $regex: search, $options: "i" } },
+          { description: { $regex: search, $options: "i" } },
+        ],
+        difficultyLevel,
+      };
+
+      const result = await assignmentCollection.find(query).toArray();
+      res.json(result);
+    });
+
+
+
+    //delete an assignment who created
+    app.delete("/create/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await assignmentCollection.deleteOne(query);
-      res.json(result)
-    })
+      res.json(result);
+    });
 
+    // users collection
+    // users data
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
 
-
-    // users collection 
-      // users data 
-      app.get('/users',async(req,res)=>{
-        const result = await userCollection.find().toArray();
-        res.send(result)
-    })
-
-    app.get('/users/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id:new ObjectId(id)}
-        const result = await userCollection.findOne(query);
-        res.send(result)
-    })
-
-    app.post('/users',async(req,res)=>{
-        const user = req.body;
-        // console.log(user)
-        const result = await userCollection.insertOne(user);
-        res.send(result)
-    })
-
-
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // console.log(user)
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // take assighnment by user
 
-    app.post('/submitAssignment',async(req,res)=>{
+    app.post("/submitAssignment", async (req, res) => {
       const assignment = req.body;
       const result = await assignmentSubmissionCollection.insertOne(assignment);
       res.json(result);
-    })
+    });
 
-    app.get('/submitAssignment',async(req,res)=>{
+    app.get("/submitAssignment", async (req, res) => {
       const cursor = assignmentSubmissionCollection.find({});
       const result = await cursor.toArray();
       res.json(result);
-    })
+    });
     // get all assignments submitted by a user
-    app.get('/submitAssignment/:email',async(req,res)=>{
+    app.get("/submitAssignment/:email", async (req, res) => {
       const email = req.params.email;
-      const query = {userEmail:email}
+      const query = { userEmail: email };
       const result = await assignmentSubmissionCollection.find(query).toArray();
       res.json(result);
-    })
+    });
 
-    app.put('/submitAssignment/:id',async(req,res)=>{
+    app.put("/submitAssignment/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set:{
-          status:req.body.status,
-          examinerEmail:req.body.examinerEmail,
-          examinerName:req.body.examinerName,
-          obtainedMarks:req.body.obtainedMarks,
-          feedback:req.body.feedback
-        }
-      }
-      const result = await assignmentSubmissionCollection.updateOne(query,updateDoc);
-      res.json(result)
-    })
-
-
-
+        $set: {
+          status: req.body.status,
+          examinerEmail: req.body.examinerEmail,
+          examinerName: req.body.examinerName,
+          obtainedMarks: req.body.obtainedMarks,
+          feedback: req.body.feedback,
+        },
+      };
+      const result = await assignmentSubmissionCollection.updateOne(
+        query,
+        updateDoc
+      );
+      res.json(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -178,5 +216,5 @@ app.listen(port, () => {
   console.log("My simple server is running at", port);
 });
 
-// 
-// 
+//
+//
